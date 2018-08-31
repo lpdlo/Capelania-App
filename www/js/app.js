aa94f0931f57bbb6d9c3d4ce640aba295d3f560b
maatheusgois <matheusgoislimasilva@gmail.com>
2018-08-31 17:42:48 -0300

    Css

--- www/js/app.js
+++ www/js/app.js
@@ -8,32 +8,32 @@ var app = new Framework7({
   id: 'br.mackenzie.lfs.App',
   root: '#app',
   theme: 'auto',
-  routes: [{
-    path: '/about/',
-    url: './pages/about.html',
-  }, {
-    path: '/menu/',
-    componentUrl: './pages/menu.html'
-  }, {
-    path: '/index/',
-    url: './index.html',
-  }, {
-    path: '/abc/',
-    componentUrl: './pages/abc.html'
-  }, {
-    path: '/abu/',
-    componentUrl: './pages/abu.html'
-  }, {
-    path: '/bola/',
-    componentUrl: './pages/bola.html'
-  }, {
-    path: '/ge/',
-    componentUrl: './pages/ge.html'
-  }, {
-    path: '/pock/',
-    componentUrl: './pages/pock.html'
-  }
-]
+
+
+  routes: [
+    {
+      path: '/about/',
+      url: './pages/about.html',
+    }, {
+      path: '/menu/',
+      componentUrl: './pages/menu.html'
+    }, {
+      path: '/abc/',
+      componentUrl: './pages/abc.html'
+    }, {
+      path: '/abu/',
+      componentUrl: './pages/abu.html'
+    }, {
+      path: '/bola/',
+      componentUrl: './pages/bola.html'
+    }, {
+      path: '/ge/',
+      componentUrl: './pages/ge.html'
+    }, {
+      path: '/pock/',
+      componentUrl: './pages/pock.html'
+    }
+  ]
 });
 
 var mainView = app.views.create('.view-main');
